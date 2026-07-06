f93077b649b6bd62a17523f5dc09ea1333d84cc8
Mohammad Umar <mdirfan0645@gmail.com>
2026-07-06 17:14:23 +0530

    langChain Completed

--- phase1/index.js
+++ phase1/index.js
@@ -1,33 +1,78 @@
-import express from 'express';
-import dotenv from 'dotenv';
-// Ensure you have installed: npm install @google/genai
-import { GoogleGenAI } from '@google/genai'; 
+import express from "express";
+import dotenv from "dotenv";
 
+import { GoogleGenAI } from "@google/genai";
+import { ChatGoogleGenerativeAI } from "@langchain/google-genai"
+import {ChatGroq} from "@langchain/groq"
 dotenv.config();
 
 const app = express();
-app.use(express.urlencoded({
-    extended:false
-}))
-app.use(express.json())
-const ai = new GoogleGenAI({
-    apiKey:process.env.GEMINI_API_KEY
-});
+app.use(
+  express.urlencoded({
+    extended: false,
+  }),
+);
+app.use(express.json());
 
+//without the langchain
 
+// const ai = new GoogleGenAI({
+//   apiKey: process.env.GEMINI_API_KEY,
+// });
+// app.post("/ai", async (req, res) => {
+//   const { input } = req.body;
+//   const response = await ai.models.generateContent({
+//     model: "gemini-2.5-flash",
+//     contents: [
+//       {
+//         role: "model",
+//         parts: [{ text: "You are an ai assitance and Your name is Ali. If you do not know the answer then do not give the incorrect answer" }],
+//       },
+//       {
+//         role: "user",
+//         parts: [{ text: input }],
+//       },
+//     ],
+//   });
+//   return res.status(200).json({ msg: `${response.text}`});
+// });
 
-app.post('/ai',async(req,res)=>{
+// with the llangchain
+
+const llm=new ChatGroq({
+    model:"openai/gpt-oss-120b",
+    temperature:0.7,
+    maxTokens:100,
+    maxRetries:2
+})
+
+app.use('/ai',async(req,res)=>{
     const {input}=req.body
-   
-   const response=await ai.interactions.create({
-     model: "gemini-3.5-flash",
-    input: input,
-   })
-   return res.status(200).json({msg:`${response.output_text}`})
-
+    const response=await llm.invoke([
+        [
+            'system',
+            "You are an ai ai assistance and Your name is Sara.If you don't know the answer please do not give the answer"
+        ],
+        [
+            "human", input
+        ]
+    ])
+    return res.status(200).json({msg:`${response.content}
+        `})
 })
 
 app.use(express.json());
 app.listen(process.env.PORT, () => {
-    console.log(`Server started at port: ${process.env.PORT}`);
+  console.log(`Server started at port: ${process.env.PORT}`);
 });
+// the sdk api has many version but the official version of the that is currently availabe is the api.interaction version given below is the interaction version but the we are going to use the ai.models version
+//   const interaction = await ai.interactions.create({
+//     model: "gemini-2.5-flash",
+//     system_instruction: "You are an AI assistant and your name is Ali.",
+//     input: [
+//       {
+//         type: "text",
+//         text: input,
+//       },
+//     ],
+//   });
